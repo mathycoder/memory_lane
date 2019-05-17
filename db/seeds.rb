719134ac1fc459a47bb06da4fb2eda3607591d2e
Adam Sarli <adam.sarli@gmail.com>
2019-05-17 14:29:47 -0400

    made attributes of Instance conditionally display with ERB

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,38 @@ first_memory = Memory.create(title: "Our first date", location: "Central Park", 
 first_memory.lane = our_lane
 first_memory.save
 
-#create my recollection
-adam_recollection = Recollection.create(anecdote: "You talked about working in global health.  I knew was out of my element, so I decided to ask you lots of questions. ")
-adam_recollection.user = adam
-adam_recollection.memory = first_memory
-adam_recollection.quote = "I'm going to kill you. ~Cathy"
-adam_recollection.save
+    #create my recollection
+    adam_recollection = Recollection.create(anecdote: "You talked about working in global health.  I knew was out of my element, so I decided to ask you lots of questions. ")
+    adam_recollection.user = adam
+    adam_recollection.memory = first_memory
+    adam_recollection.quote = "\"I'm going to kill you.\" ~Cathy"
+    adam_recollection.save
 
-#create Cathy's recollection
-cathy_recollection = Recollection.create(anecdote: "You talked about video games.  At one point you offered me the chance to take the fork in the road after telling me you had played Zelda all weekend.")
-cathy_recollection.user = cathy
-cathy_recollection.memory = first_memory
-cathy_recollection.joke = "I kept joking about how I loved killing mice."
-cathy_recollection.save
+    #create Cathy's recollection
+    cathy_recollection = Recollection.create(anecdote: "You talked about video games.  At one point you offered me the chance to take the fork in the road after telling me you had played Zelda all weekend.")
+    cathy_recollection.user = cathy
+    cathy_recollection.memory = first_memory
+    cathy_recollection.joke = "I kept joking about how I loved killing mice."
+    cathy_recollection.save
+
+
+#create second memory
+second_memory = Memory.create(title: "Our second date", location: "Washington Square Park", date: "May 14th, 2017")
+second_memory.lane = our_lane
+second_memory.save
+
+    #create my recollection
+    adam_recollection = Recollection.create
+    adam_recollection.user = adam
+    adam_recollection.memory = second_memory
+    adam_recollection.anecdote = "You tried talking to a squirrel, who wouldn't cooperate.  The rain came suddenly, and we ran to a bar.  I remember wanting to play the piano for you."
+    adam_recollection.quote = "\"squirrel noises\" ~Cathy"
+    adam_recollection.save
+
+    #create Cathy's recollection
+    cathy_recollection = Recollection.create
+    cathy_recollection.user = cathy
+    cathy_recollection.memory = second_memory
+    cathy_recollection.anecdote = "I remember running in the rain to a bar.  We didn't know where we were going...we were just trying to catch some refuge.  I made some joke about the sex shop near the bar that got you laughing.  The bar was so empty...just a bunch of old people and then us sitting down."
+    cathy_recollection.joke = "There was a really loud lady at the bar.  At one point you joked about playing the piano."
+    cathy_recollection.save
