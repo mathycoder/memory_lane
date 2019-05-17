3291d267c71b86de0fc303e4b2627c76844ea6d2
Adam Sarli <adam.sarli@gmail.com>
2019-05-17 17:45:47 -0400

    Edited RecollectionsController path to post a new recollection and attach it to an existing memory.

--- app/controllers/recollections_controller.rb
+++ app/controllers/recollections_controller.rb
@@ -3,7 +3,23 @@ require_relative "./application_controller.rb"
 class RecollectionsController < ApplicationController
 
   get '/users/:user_id/memories/:memory_id/recollections/new' do
-    binding.pry
+    @memory = Memory.find(params[:memory_id])
+    @user = User.find(params[:user_id])
+    erb :'recollections/new'
+  end
+
+  post '/users/:user_id/memories/:memory_id/recollections' do
+    recollection = Recollection.create
+    recollection.user = User.find(params[:user_id])
+
+    memory = Memory.find(params[:memory_id])
+    recollection.memory = memory
+    recollection.anecdote = params[:recollection][:anecdote] if !params[:recollection][:anecdote].empty?
+    recollection.joke = params[:recollection][:joke] if !params[:recollection][:joke].empty?
+    recollection.quote = params[:recollection][:quote] if !params[:recollection][:quote].empty?
+    recollection.save
+
+    redirect "/users/#{params[:user_id]}/lanes/#{memory.lane.id}"
   end
 
 end
