395ab1a11358a99401c3bc0c6be6f86a415ea059
Adam Sarli <adam.sarli@gmail.com>
2019-05-17 16:06:59 -0400

    modified post /memories to create a recollection instance

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -14,6 +14,15 @@ class MemoriesController < ApplicationController
 
   post '/users/:user_id/memories' do
     memory = Memory.create(params[:memory])
+
+    recollection = Recollection.create
+    recollection.user = User.find(params[:user_id])
+    recollection.memory = memory
+    recollection.anecdote = params[:recollection][:anecdote] if !params[:recollection][:anecdote].empty?
+    recollection.joke = params[:recollection][:joke] if !params[:recollection][:joke].empty?
+    recollection.quote = params[:recollection][:quote] if !params[:recollection][:quote].empty?
+    recollection.save
+
     redirect "/users/#{params[:user_id]}/lanes/#{memory.lane.id}"
   end
 end
