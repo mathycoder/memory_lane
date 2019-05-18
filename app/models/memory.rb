51d7b7b24249acd08c055ab30038b034f542b235
Adam Sarli <adam.sarli@gmail.com>
2019-05-18 15:48:19 -0400

    Displayed date using #ordinalize

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/integer/inflections'
+
 class Memory < ActiveRecord::Base
   belongs_to :lane
   has_many :recollections
@@ -9,6 +11,12 @@ class Memory < ActiveRecord::Base
 
   def creator
     User.find(self.creator_user_id)
+  end
+
+  def display_date
+    #self.date.httpdate.split(" 00:00:00").first
+    self.date.strftime("%A, %B #{self.date.day.ordinalize}, %Y")
+    #
   end
 
 end
