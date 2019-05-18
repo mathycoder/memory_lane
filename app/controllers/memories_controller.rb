d10d8dd380bf28c610fbeee585206850c1e37a2b
Adam Sarli <adam.sarli@gmail.com>
2019-05-18 12:13:41 -0400

    Added a 'posted by' to the memory display in Memory index and Lane show page.

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -15,10 +15,6 @@ class MemoriesController < ApplicationController
   post '/users/:user_id/memories' do
     memory = Memory.create(params[:memory])
 
-    #this might be redundant, but I need to make sure each memory knows who it's users are,
-    #even though it might be enough to pull it from the lane.
-
-
     recollection = Recollection.create
     recollection.user = User.find(params[:user_id])
     recollection.memory = memory
