9a3bba3e394d8533d75ee6eac647497c4528ef05
Adam Sarli <adam.sarli@gmail.com>
2019-05-20 13:30:04 -0400

    Added /signup controller action and create_user.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:user_id])
     erb :'users/show'
   end
+
+  get '/signup' do
+  # if logged_in?
+  #   redirect '/tweets'
+  # end
+
+  erb :'users/create_user'
+  end
+
+  post '/signup' do
+    user = User.create(params[:user])
+    user.profile_pic_file_path = params[:profile_pic][:filename]
+    user.save
+    file = params[:profile_pic][:tempfile]
+    File.open("./public/#{user.profile_pic_file_path}", 'wb') do |f|
+      f.write(file.read)
+    end
+
+    redirect "/users/#{user.id}"
+  end
+
 end
