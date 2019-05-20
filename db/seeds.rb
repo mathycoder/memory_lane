68188b9b18bfa8b605afc650ea7587fa6e308732
Adam Sarli <adam.sarli@gmail.com>
2019-05-20 10:10:41 -0400

    updated seed data for new photo objects; made it so you can add multiple pictures to a memory

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,14 @@ first_memory.save
     adam_recollection.user = adam
     adam_recollection.memory = first_memory
     adam_recollection.quote = "\"I'm going to kill you.\" ~Cathy"
-    adam_recollection.image = "seed_images/IMG_7457.JPG"
     adam_recollection.save
+
+    #create my photo
+    adam_photo = Photo.create()
+    adam_photo.file_path = "seed_images/IMG_7457.JPG"
+    adam_photo.user = adam
+    adam_photo.memory = first_memory
+    adam_photo.save
 
     #create Cathy's recollection
     cathy_recollection = Recollection.create(anecdote: "You talked about video games.  At one point you offered me the chance to take the fork in the road after telling me you had played Zelda all weekend.")
@@ -55,8 +61,13 @@ second_memory.save
     cathy_recollection.memory = second_memory
     cathy_recollection.anecdote = "I remember running in the rain to a bar.  We didn't know where we were going...we were just trying to catch some refuge.  I made some joke about the sex shop near the bar that got you laughing.  The bar was so empty...just a bunch of old people and then us sitting down."
     cathy_recollection.joke = "There was a really loud lady at the bar.  At one point you joked about playing the piano."
-    cathy_recollection.image = "seed_images/IMG_1833.JPG"
     cathy_recollection.save
+
+    cathy_photo = Photo.create()
+    cathy_photo.file_path = "seed_images/IMG_1833.JPG"
+    cathy_photo.user = cathy
+    cathy_photo.memory = second_memory
+    cathy_photo.save
 
 #### New lane ####
 new_lane = Lane.create()
@@ -79,8 +90,15 @@ first_memory.save
     adam_recollection.memory = first_memory
     adam_recollection.anecdote = "We turned Corbitt into 'Alex' by putting 1/4 of a can of hair product into his hair."
     adam_recollection.joke = "'Alex' is out of control."
-    adam_recollection.image = "seed_images/IMG_1831.JPG"
     adam_recollection.save
+
+    adam_photo = Photo.create()
+    adam_photo.file_path = "seed_images/IMG_1831.JPG"
+    adam_photo.user = adam
+    adam_photo.memory = first_memory
+    adam_photo.save
+
+
 
     #alex's recollection
     alex_recollection = Recollection.create()
@@ -88,8 +106,13 @@ first_memory.save
     alex_recollection.memory = first_memory
     alex_recollection.anecdote = "I remember playing for hours at the Craps table, while Weiss and I pretended we were true gamblers."
     alex_recollection.quote = "\"Put it all on black.\" ~Eric"
-    alex_recollection.image = "seed_images/IMG_1832.JPG"
     alex_recollection.save
+
+    alex_photo = Photo.create()
+    alex_photo.file_path = "seed_images/IMG_1832.JPG"
+    alex_photo.user = alex
+    alex_photo.memory = first_memory
+    alex_photo.save
 
     #nathan's recollection
     nathan_recollection = Recollection.create()
@@ -98,8 +121,13 @@ first_memory.save
     nathan_recollection.anecdote = "I remember playing blackjack with Sarli and charming the table.  It was the first time I ever liked gambling."
     nathan_recollection.quote = "\"What should I do, Rhonda?\" ~me"
     nathan_recollection.joke = "The dealer thought Sarli's name was Charlie."
-    nathan_recollection.image = "seed_images/IMG_1833.JPG"
     nathan_recollection.save
+
+    nathan_photo = Photo.create()
+    nathan_photo.file_path = "seed_images/IMG_8241.JPG"
+    nathan_photo.user = nathan
+    nathan_photo.memory = first_memory
+    nathan_photo.save
 
     #eric's recollection
     eric_recollection = Recollection.create()
@@ -137,8 +165,13 @@ second_memory.save
     adam_recollection.memory = second_memory
     adam_recollection.anecdote = "Weiss and I were combing the crowd of thousands, hoping not to miss you.  Of course, it was impossible to miss you guys; you came bounding up to us like kangaroos, among a sea of exhausted people slogging along.  How the hell did you find the energy?"
     adam_recollection.quote = "\"F@%$ yeah!\" ~Alex"
-    adam_recollection.image = "seed_images/IMG_1830.PNG"
     adam_recollection.save
+
+    adam_photo = Photo.create()
+    adam_photo.file_path = "seed_images/IMG_1830.PNG"
+    adam_photo.user = adam
+    adam_photo.memory = second_memory
+    adam_photo.save
 
     #eric's recollection
     eric_recollection = Recollection.create()
@@ -146,5 +179,10 @@ second_memory.save
     eric_recollection.memory = second_memory
     eric_recollection.anecdote = "Seeing the two of you running towards us with so much energy.  I remember how much it inspired me to be healthier.  Later at a restaurant with Adam, we tracked your progress on my phone.  I kept hoping you would break 4 hours...and you did!"
     eric_recollection.quote = "\"I hope they break 4 hours!\" ~Eric"
-    eric_recollection.image = "seed_images/IMG_1829.PNG"
     eric_recollection.save
+
+    eric_photo = Photo.create()
+    eric_photo.file_path = "seed_images/IMG_1829.PNG"
+    eric_photo.user = eric
+    eric_photo.memory = second_memory
+    eric_photo.save
