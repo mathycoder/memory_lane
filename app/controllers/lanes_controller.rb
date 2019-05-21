4b36e33b1963e9de73746c8cd448d0abf5882d85
Adam Sarli <adam.sarli@gmail.com>
2019-05-21 19:14:11 -0400

    Controller get and post action for creating a new lane

--- app/controllers/lanes_controller.rb
+++ app/controllers/lanes_controller.rb
@@ -18,13 +18,26 @@ class LanesController < ApplicationController
   get '/users/:user_id/lanes/new' do
     @user = User.find(params[:user_id])
     if logged_in? && current_user == @user
-      @users = User.alphabetize 
+      @users = User.alphabetize
       erb :'lanes/new'
     elsif !logged_in?
       redirect :'/login'
     else
       erb :'users/noaccess'
     end
+  end
+
+  post '/users/:user_id/lanes' do
+    @user = User.find(params[:user_id])
+    lane = Lane.create()
+    lane.users << @user
+    params[:users].each do |user_id|
+      user = User.find(user_id)
+      lane.users << user
+    end
+    lane.save
+
+    redirect "/users/#{@user.id}/lanes/#{lane.id}"
   end
 
 
