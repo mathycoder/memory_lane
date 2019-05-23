34021737788a3719d588b78b90175dea2bd69a06
Adam Sarli <adam.sarli@gmail.com>
2019-05-23 13:56:25 -0400

    Created #alphabetized_firstnames for class Lane

--- app/models/lane.rb
+++ app/models/lane.rb
@@ -2,5 +2,13 @@ class Lane < ActiveRecord::Base
   has_many :user_lanes
   has_many :users, through: :user_lanes
   has_many :memories
-  has_many :recollections, through: :memories 
+  has_many :recollections, through: :memories
+
+
+  def alphabetized_firstnames(current_user)
+    list = self.users.sort_by{|user| user.name}.map{|user| user.name.split(" ").first}
+    list.delete(current_user.name.split(" ").first)
+    list
+  end
+
 end
