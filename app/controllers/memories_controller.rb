e68665ed83019eabe534f7d0c6ba01a0377d9b51
Adam Sarli <adam.sarli@gmail.com>
2019-05-30 11:41:35 -0400

    Decided to remove the ability to create a recollection while creating a memory.  Changed the memory controller and the memory/new show page

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -27,17 +27,6 @@ class MemoriesController < ApplicationController
     memory.timestamp = DateTime.now
     memory.save
 
-    if !params[:recollection][:anecdote].empty? || !params[:recollection][:joke].empty? || !params[:recollection][:quote].empty?
-      recollection = Recollection.create
-      recollection.user = User.find(params[:user_id])
-      recollection.memory = memory
-      recollection.anecdote = params[:recollection][:anecdote] if !params[:recollection][:anecdote].empty?
-      recollection.joke = params[:recollection][:joke] if !params[:recollection][:joke].empty?
-      recollection.quote = params[:recollection][:quote] if !params[:recollection][:quote].empty?
-      recollection.timestamp = DateTime.now
-      recollection.save
-    end 
-
     flash[:alert] = "New Memory successfully added"
     redirect "/users/#{params[:user_id]}/lanes/#{memory.lane.id}"
   end
