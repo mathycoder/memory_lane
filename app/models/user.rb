da0cd919a791be60540f1ef679b3479adc1580fd
Adam Sarli <adam.sarli@gmail.com>
2019-06-04 09:24:34 -0400

    changed user has_many memories and recollections so that it doesn't go through memories...doesn't seem to affect anything on the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   has_many :user_lanes
   has_many :lanes, through: :user_lanes
   has_many :memories, through: :lanes
-  has_many :recollections, through: :memories
-  has_many :images, through: :memories
+  #has_many :recollections, through: :memories
+  has_many :recollections
+  #has_many :images, through: :memories
+  has_many :images
 
   has_secure_password
 
