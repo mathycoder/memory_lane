59283be4214e8bc0e0ac96e9b2b2c44401965b17
Adam Sarli <adam.sarli@gmail.com>
2019-06-04 11:48:40 -0400

    you can now reveal hidden lanes

--- app/controllers/user_lanes_controller.rb
+++ app/controllers/user_lanes_controller.rb
@@ -2,6 +2,13 @@ require_relative "./application_controller.rb"
 
 class UserLanesController < ApplicationController
   patch '/lanes/:lane_id' do
-    binding.pry 
+    create_instance_variables(params)
+    current_user.user_lanes.each {|user_lane| user_lane.update(hidden: true) if user_lane.lane == @lane}
+    redirect "/lanes"
+  end
+
+  patch '/lanes' do
+    current_user.user_lanes.each {|user_lane| user_lane.update(hidden: false)}
+    redirect "/lanes"
   end
 end
